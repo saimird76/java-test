62b2f616626897e3b7bac45ba498a4345ce6f428
saimird76 <56480697+saimird76@users.noreply.github.com>
2019-11-23 18:49:26 +0100

    Update HandGesture.java

--- HandGesture.java
+++ HandGesture.java
@@ -1,24 +1,29 @@
 package rps;
+/**
+ * 
+ * @author SAIMIR
+ * 
+ */
 
 import java.util.Arrays;
 import java.util.List;
 
 public enum HandGesture {
 	R, P, S;
-
 	private List<HandGesture> losesToList;
 
 	public boolean losesTo(HandGesture other) {
 		return losesToList.contains(other);
 	}
-	
+
 	static {
 		S.losesToList = Arrays.asList(R);
 		R.losesToList = Arrays.asList(P);
 		P.losesToList = Arrays.asList(S);
 	}
-	
+
 	public static HandGesture randomHG() {
 		return HandGesture.values()[(int) (Math.random() * HandGesture.values().length)];
 	}
+
 }
